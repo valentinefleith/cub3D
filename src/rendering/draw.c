5a24b080b74e0473d8867f74257ec2cc16da0b49
lucie vallee <vallee.lucie99@gmail.com>
2025-03-18 19:42:41 +0100

    wall shading

--- src/rendering/draw.c
+++ src/rendering/draw.c
@@ -12,6 +12,25 @@
 
 #include "cub3d.h"
 
+static int	shading_color(int color, double dist, double max_dist)
+{
+	int		r;
+	int		g;
+	int		b;
+	double	intensity;
+
+	r = (color >> 16) & 0xFF;
+	g = (color >> 8) & 0xFF;
+	b = color & 0xFF;
+	intensity = 0.6 + (dist / max_dist);
+	if (intensity > 1.0)
+		intensity = 1.0;
+	r *= intensity;
+	g *= intensity;
+	b *= intensity;
+	return ((r << 16) | (g << 8) | b);
+}
+
 void	draw_wall(t_maze *maze, t_img texture, double wall_height, int x)
 {
 	int		y;
@@ -28,11 +47,13 @@ void	draw_wall(t_maze *maze, t_img texture, double wall_height, int x)
 	scale = (double)texture.height / (wall_height - 1);
 	draw_ceilling(maze, x, y);
 	texture.y = ((double)y - ((HEIGHT / 2) - (wall_height / 2))) * scale;
+	double shading_factor = wall_height / (double)HEIGHT;
+    if (shading_factor > 1.0)
+		shading_factor = 1.0;
 	while (y < end_y)
 	{
 		px_color = get_px_color(texture, texture.x, texture.y);
-		// if (wall_height < 90.0 && !maze->horizontal_point)
-		// 	px_color = (px_color >> 1) & 8355711;
+		px_color = shading_color(px_color, wall_height, HEIGHT);
 		if (px_color > 0)
 			my_mlx_pixel_put(&(maze->img), x, y, px_color);
 		texture.y += scale;
@@ -53,11 +74,14 @@ void	draw_floor(t_maze *maze, int x, int start)
 void	draw_ceilling(t_maze *maze, int x, int end)
 {
 	int	start;
+	int	color;
 
 	start = 0;
+	color = maze->map.ceilling_color;
 	while (start <= end)
 	{
-		my_mlx_pixel_put(&(maze->img), x, start, maze->map.ceilling_color);
+		color = shading_color(color, (double)end, (double)start);
+		my_mlx_pixel_put(&(maze->img), x, start, color);
 		start++;
 	}
 }
